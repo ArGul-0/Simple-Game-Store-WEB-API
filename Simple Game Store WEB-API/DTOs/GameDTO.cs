0abdb9d3c8245bff0c86298f611a839a98a7c451
ArGul <argul.mail@gmail.com>
2025-12-26 18:29:42 +0300

    Created DTOs folder and added first DTO - GameDTO.

---
+++ Simple Game Store WEB-API/DTOs/GameDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_Game_Store_WEB_API.DTOs
+{
+    public record class GameDTO(
+        int ID,
+        string Name,
+        string Genre,
+        decimal Price,
+        DateOnly ReleaseDate);
+}
