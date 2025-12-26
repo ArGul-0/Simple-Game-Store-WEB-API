215e9727ea2a8986e5c0ffed7922d820c1001113
ArGul <argul.mail@gmail.com>
2025-12-26 18:35:42 +0300

    Added Games in memory.

--- Simple Game Store WEB-API/Program.cs
+++ Simple Game Store WEB-API/Program.cs
@@ -1,4 +1,6 @@
 
+using Simple_Game_Store_WEB_API.DTOs;
+
 namespace Simple_Game_Store_WEB_API
 {
     public class Program
@@ -24,6 +26,15 @@ namespace Simple_Game_Store_WEB_API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            List<GameDTO> games = 
+            [
+                new GameDTO(1, "Civilization VI", "4X", 59.99m, new DateOnly(2016, 10, 21)),
+                new GameDTO(2, "Stellaris", "4X", 39.99m, new DateOnly(2016, 5, 9)),
+                new GameDTO(3, "Cities: Skylines", "Management", 29.99m, new DateOnly(2015, 3, 10)),
+                new GameDTO(4, "Planet Zoo", "Management", 44.99m, new DateOnly(2019, 11, 5)),
+                new GameDTO(5, "Endless Legend", "4X", 29.99m, new DateOnly(2014, 9, 18))
+            ];
 
 
 
