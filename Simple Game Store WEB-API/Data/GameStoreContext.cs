35d4b338ebd493dd86eb1f8914c8e9c2eca5bef2
ArGul <argul.mail@gmail.com>
2026-01-07 10:27:06 +0300

    Added GameStoreContext

---
+++ Simple Game Store WEB-API/Data/GameStoreContext.cs
@@ -0,0 +1,11 @@
+﻿using Simple_Game_Store_WEB_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Simple_Game_Store_WEB_API.Data
+{
+    public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+    {
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Genre> Genres => Set<Genre>();
+    }
+}
