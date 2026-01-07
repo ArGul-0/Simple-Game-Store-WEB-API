6633da39f20cf00ab7c0e7d6630bb4b224ec3414
ArGul <argul.mail@gmail.com>
2026-01-07 09:56:30 +0300

    Started add Entity Framework Core support, added Entities...

---
+++ Simple Game Store WEB-API/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace Simple_Game_Store_WEB_API.Entities
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public int GenreID { get; set; }
+        public Genre? Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
---
+++ Simple Game Store WEB-API/Entities/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple_Game_Store_WEB_API.Entities
+{
+    public class Genre
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+    }
+}
