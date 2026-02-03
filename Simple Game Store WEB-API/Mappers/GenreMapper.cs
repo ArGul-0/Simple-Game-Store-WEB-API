068aba988590af1a081531214e7c190727dee619
ArGul <argul.mail@gmail.com>
2026-02-03 12:00:58 +0300

    Created IGenreMapper and GenreMapper.

---
+++ Simple Game Store WEB-API/Mappers/GenreMapper.cs
@@ -0,0 +1,16 @@
+﻿using Simple_Game_Store_WEB_API.Entities;
+using Simple_Game_Store_WEB_API.DTOs;
+
+namespace Simple_Game_Store_WEB_API.Mappers
+{
+    public class GenreMapper : IGenreMapper
+    {
+        public GenreDTO ToDTO(Genre genre)
+        {
+            return new GenreDTO(
+                ID: genre.ID,
+                Name: genre.Name
+            );
+        }
+    }
+}
---
+++ Simple Game Store WEB-API/Mappers/IGenreMapper.cs
@@ -0,0 +1,14 @@
+﻿using Simple_Game_Store_WEB_API.Entities;
+using Simple_Game_Store_WEB_API.DTOs;
+
+namespace Simple_Game_Store_WEB_API.Mappers
+{
+    public interface IGenreMapper
+    {
+        /// <summary>
+        /// Converts Genre Entity to GenreDTO
+        /// </summary>
+        /// <returns>GenreDTO</returns>
+        public GenreDTO ToDTO(Genre genre);
+    }
+}
