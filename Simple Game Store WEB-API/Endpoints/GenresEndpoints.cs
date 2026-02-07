af58f12289d50bffc748ad9174c1082aaca4bd81
ArGul <argul.mail@gmail.com>
2026-02-07 14:21:07 +0300

    Created Get Genre endpoint.

--- Simple Game Store WEB-API/Endpoints/GenresEndpoints.cs
+++ Simple Game Store WEB-API/Endpoints/GenresEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using Simple_Game_Store_WEB_API.Mappers;
+﻿using Simple_Game_Store_WEB_API.Entities;
+using Simple_Game_Store_WEB_API.Mappers;
 using Simple_Game_Store_WEB_API.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,8 @@ namespace Simple_Game_Store_WEB_API.Endpoints
 {
     public static class GenresEndpoints // Static Class For Genres Endpoints
     {
+        const string GetGenreEndpointName = "GetGenre"; // Constant For The Get Genre Endpoint Name
+
         /// <summary>
         /// Maps The Genres Endpoints To The Web Application
         /// </summary>
@@ -28,6 +31,16 @@ namespace Simple_Game_Store_WEB_API.Endpoints
                     .ToListAsync();
                 return Results.Ok(genres);
             });
+
+            // Get Genre
+            genresGroup.MapGet("/{ID}", async (int ID, GameStoreContext dbContext, IGenreMapper genreMapper) =>
+            {
+                Genre? genre = await dbContext.Genres
+                    .AsNoTracking() // Avoid Tracking For Read-Only Operation, Improves Performance
+                    .FirstOrDefaultAsync(g => g.ID == ID);
+
+                return genre is not null ? Results.Ok(genreMapper.ToDTO(genre)) : Results.NotFound();
+            }).WithName(GetGenreEndpointName);
 
             return genresGroup; // Return The Group For Further Configuration If Needed
         }
