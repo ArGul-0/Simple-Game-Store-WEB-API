a6a6131882fadf56c1cd93a78be912611dddbc04
ArGul <argul.mail@gmail.com>
2026-02-08 10:55:10 +0300

    Created UpdateGenreDTO.

---
+++ Simple Game Store WEB-API/DTOs/UpdateGenreDTO.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_Game_Store_WEB_API.DTOs
+{
+    public record class UpdateGenreDTO( // Data Transfer Object For Updating An Existing Genre
+        [Required][StringLength(50)] string Name);
+}
