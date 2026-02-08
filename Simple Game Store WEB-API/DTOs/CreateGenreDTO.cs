43880136cf2cefaf55599f43efc2d7d17f87971e
ArGul <argul.mail@gmail.com>
2026-02-08 10:54:02 +0300

    Created CreateGenreDTO

---
+++ Simple Game Store WEB-API/DTOs/CreateGenreDTO.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_Game_Store_WEB_API.DTOs
+{
+    public record class CreateGenreDTO( // Data Transfer Object For Creating A New Genre
+        [Required][StringLength(50)] string Name);
+}
