0e2953f53845e936ce179bb18de66982191aabd0
ArGul <argul.mail@gmail.com>
2026-02-08 11:03:05 +0300

    Added contract methods to Converts CreateGenreDTO To Genre Entity and Converts UpdateGenreDTO To Genre Entity in IGenreMapper.

--- Simple Game Store WEB-API/Mappers/IGenreMapper.cs
+++ Simple Game Store WEB-API/Mappers/IGenreMapper.cs
@@ -6,6 +6,19 @@ namespace Simple_Game_Store_WEB_API.Mappers
     public interface IGenreMapper
     {
         /// <summary>
+        /// Converts CreateGenreDTO To Genre Entity
+        /// </summary>
+        /// <returns>Genre</returns>
+        public Genre ToEntity(CreateGenreDTO createGenreDTO);
+        /// <summary>
+        /// Converts UpdateGenreDTO To Genre Entity
+        /// </summary>
+        /// <returns>Genre</returns>
+        public Genre ToEntity(UpdateGenreDTO updateGenreDTO);
+
+
+
+        /// <summary>
         /// Converts Genre Entity to GenreDTO
         /// </summary>
         /// <returns>GenreDTO</returns>
