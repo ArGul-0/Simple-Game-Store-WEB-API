7ff934d382b0151db28b5742716f6fffa03b09db
ArGul <argul.mail@gmail.com>
2026-02-16 09:04:35 +0300

    Created User entity class.

--- Simple Game Store WEB-API/Entities/User.cs
+++ Simple Game Store WEB-API/Entities/User.cs
@@ -1,6 +1,10 @@
 ﻿namespace Simple_Game_Store_WEB_API.Entities
 {
-    public class User
+    public class User // User Entity Class
     {
+        public int ID { get; set; }
+        public required string UserName { get; set; }
+        public required string UserEmail { get; set; }
+        public required string HashedUserPassword { get; set; }
     }
 }
