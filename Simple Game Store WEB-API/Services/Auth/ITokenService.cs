7309a9748ef38ee6405ddfd6bbc48faea0df3f8a
ArGul <argul.mail@gmail.com>
2026-02-16 09:17:58 +0300

    Created ITokenService interface.

--- Simple Game Store WEB-API/Services/Auth/ITokenService.cs
+++ Simple Game Store WEB-API/Services/Auth/ITokenService.cs
@@ -1,6 +1,13 @@
-﻿namespace Simple_Game_Store_WEB_API.Services.Auth
+﻿using Simple_Game_Store_WEB_API.Entities;
+
+namespace Simple_Game_Store_WEB_API.Services.Auth
 {
     public interface ITokenService
     {
+        /// <summary>
+        /// Generates A JWT Access Token For The Specified User.
+        /// </summary>
+        /// <returns>The Generated JWT Access Token (String).</returns>
+        public string GenerateAccessToken(User user);
     }
 }
