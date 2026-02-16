73f4eb7c5b258b76308dee08c438f660105924f9
ArGul <argul.mail@gmail.com>
2026-02-16 09:21:27 +0300

    Created IPasswordHasher interface.

--- Simple Game Store WEB-API/Services/Auth/IPasswordHasher.cs
+++ Simple Game Store WEB-API/Services/Auth/IPasswordHasher.cs
@@ -2,5 +2,15 @@
 {
     public interface IPasswordHasher
     {
+        /// <summary>
+        /// Hashes The Provided Password Using A Secure Hashing Algorithm (e.g., Argon2, BCrypt, PBKDF2).
+        /// </summary>
+        /// <returns>The Hashed Password (String).</returns>
+        public string HashPassword(string password);
+        /// <summary>
+        /// Verifies The Provided Password Against The Stored Hashed Password. Returns True If The Password Is Valid, Otherwise False.
+        /// </summary>
+        /// <returns>True If The Password Is Valid, Otherwise False.</returns>
+        public bool VerifyPassword(string password, string hashedPassword);
     }
 }
