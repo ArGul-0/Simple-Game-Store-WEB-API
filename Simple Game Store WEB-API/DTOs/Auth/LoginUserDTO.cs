8e9a7dd4daa4d5342714daf4a7b884c1cfc3f3c9
ArGul <argul.mail@gmail.com>
2026-02-16 09:41:01 +0300

    Created RegisterUserDTO and LoginUserDTO

--- Simple Game Store WEB-API/DTOs/Auth/LoginUserDTO.cs
+++ Simple Game Store WEB-API/DTOs/Auth/LoginUserDTO.cs
@@ -2,9 +2,8 @@
 
 namespace Simple_Game_Store_WEB_API.DTOs.Auth
 {
-    public record class RegisterUserDTO(
+    public record class LoginUserDTO(
         [Required][StringLength(20, MinimumLength = 3)] string Username,
-        [Required][EmailAddress] string Email,
         [Required][StringLength(100, MinimumLength = 6)] string Password
         );
 }
