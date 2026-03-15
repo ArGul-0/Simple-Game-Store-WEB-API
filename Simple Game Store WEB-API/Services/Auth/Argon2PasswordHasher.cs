51d4f15002ace3b1a9d44f55ee448b5f97ee769f
ArGul <argul.mail@gmail.com>
2026-03-15 11:57:46 +0300

    Now IPaswordHasher is IValueHasher.

--- Simple Game Store WEB-API/Services/Auth/Argon2PasswordHasher.cs
+++ Simple Game Store WEB-API/Services/Auth/Argon2PasswordHasher.cs
@@ -1,6 +1,15 @@
 ﻿namespace Simple_Game_Store_WEB_API.Services.Auth
 {
-    public class Argon2PasswordHasher
+    public class Argon2PasswordHasher : IValueHasher
     {
+        public string Hash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Simple Game Store WEB-API/Services/Auth/IValueHasher.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple_Game_Store_WEB_API.Services.Auth
+{
+    public interface IValueHasher
+    {
+        /// <summary>
+        /// Hashes The Provided Value Using A Secure Hashing Algorithm (e.g., Argon2, BCrypt, PBKDF2).
+        /// </summary>
+        /// <returns>The Hashed Value (String).</returns>
+        public string Hash(string value);
+        /// <summary>
+        /// Verifies The Provided Value Against The Stored Hashed Value. Returns True If The Value Is Valid, Otherwise False.
+        /// </summary>
+        /// <returns>True If The Value Is Valid, Otherwise False.</returns>
+        public bool Verify(string value, string hashedValue);
+    }
+}
