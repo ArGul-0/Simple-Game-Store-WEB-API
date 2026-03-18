68f128993ea86583345a5f04fb63252ee084539e
ArGul <argul.mail@gmail.com>
2026-03-18 18:45:50 +0300

    Created TokenService.

--- Simple Game Store WEB-API/Services/Auth/TokenService.cs
+++ Simple Game Store WEB-API/Services/Auth/TokenService.cs
@@ -1,6 +1,42 @@
-﻿namespace Simple_Game_Store_WEB_API.Services.Auth
+﻿using Simple_Game_Store_WEB_API.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Simple_Game_Store_WEB_API.Services.Auth
 {
-    public class TokenService
+    public sealed class TokenService : ITokenService
     {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateAccessToken(User user)
+        {
+            var secretKey = configuration["JwtOptions:SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.ID.ToString()),
+                    new Claim("username", user.Username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(int.Parse(configuration["JwtOptions:ExpirationHours"])),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
     }
 }
