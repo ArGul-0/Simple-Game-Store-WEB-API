bce55258461e8a20616aeb645f33377f068b4ed7
ArGul <argul.mail@gmail.com>
2026-04-04 21:19:41 +0300

    Created Error record, soon we use it between layers.

--- Simple Game Store WEB-API/Common/Results/Error.cs
+++ Simple Game Store WEB-API/Common/Results/Error.cs
@@ -1,6 +1,8 @@
 ﻿namespace Simple_Game_Store_WEB_API.Common.Results
 {
-    public class Error
+    public sealed record Error(string code, string description)
     {
+        public static readonly Error None = new Error(string.Empty, string.Empty); // Represents No Error, Can Be Used As A Default Value
+        public bool IsNone => code == string.Empty; // Check If This Error Represents No Error
     }
 }
