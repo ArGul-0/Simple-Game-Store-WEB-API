70c9e71f7991c1c9507ce8f77d0a3a9fdb91c5a6
ArGul <argul.mail@gmail.com>
2026-04-10 17:45:55 +0300

    dotnet ef migrations add NowEmailDontHashed

---
+++ Simple Game Store WEB-API/Data/Migrations/20260410144535_NowEmailDontHashed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simple_Game_Store_WEB_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NowEmailDontHashed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HashedEmail",
+                table: "Users",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "HashedEmail");
+        }
+    }
+}
