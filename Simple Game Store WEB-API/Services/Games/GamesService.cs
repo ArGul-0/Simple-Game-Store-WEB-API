0554fc022daef75a95b817c180a4c74671327061
ArGul <argul.mail@gmail.com>
2026-04-13 22:19:41 +0300

    Created IGameService and his empty GameService

---
+++ Simple Game Store WEB-API/Services/Games/GamesService.cs
@@ -0,0 +1,23 @@
+﻿using Simple_Game_Store_WEB_API.Common.Results;
+using Simple_Game_Store_WEB_API.DTOs;
+
+namespace Simple_Game_Store_WEB_API.Services.Games
+{
+    public class GamesService : IGamesService
+    {
+        public Task<Result<GameDetailsDTO>> CreateGameAsync(CreateGameDTO gameDetailsDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteGameAsync(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateGameAsync(UpdateGameDTO gameDetailsDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Simple Game Store WEB-API/Services/Games/IGamesService.cs
@@ -0,0 +1,12 @@
+﻿using Simple_Game_Store_WEB_API.Common.Results;
+using Simple_Game_Store_WEB_API.DTOs;
+
+namespace Simple_Game_Store_WEB_API.Services.Games
+{
+    public interface IGamesService
+    {
+        public Task<Result<GameDetailsDTO>> CreateGameAsync(CreateGameDTO gameDetailsDTO);
+        public Task<Result> UpdateGameAsync(UpdateGameDTO gameDetailsDTO);
+        public Task<Result> DeleteGameAsync(int ID);
+    }
+}
