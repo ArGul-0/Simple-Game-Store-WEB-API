58f73feade062946e5c562c92e478d34d14e1f2b
ArGul <argul.mail@gmail.com>
2026-04-14 12:14:30 +0300

    Deleted don't used usings in IGameService.cs

--- Simple Game Store WEB-API/Services/Games/IGamesService.cs
+++ Simple Game Store WEB-API/Services/Games/IGamesService.cs
@@ -1,7 +1,5 @@
 ﻿using Simple_Game_Store_WEB_API.Common.Results;
-using Simple_Game_Store_WEB_API.Data;
 using Simple_Game_Store_WEB_API.DTOs;
-using Simple_Game_Store_WEB_API.Mappers;
 
 namespace Simple_Game_Store_WEB_API.Services.Games
 {
