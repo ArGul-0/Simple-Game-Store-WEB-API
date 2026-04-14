510ae3b1acbdd2e18b36f3d67dbeae484280783d
ArGul <argul.mail@gmail.com>
2026-04-14 12:53:51 +0300

    Created GenreService.cs

---
+++ Simple Game Store WEB-API/Services/Genres/GenreService.cs
@@ -0,0 +1,23 @@
+﻿using Simple_Game_Store_WEB_API.Common.Results;
+using Simple_Game_Store_WEB_API.DTOs;
+
+namespace Simple_Game_Store_WEB_API.Services.Genres
+{
+    public class GenreService : IGenreService
+    {
+        public Task<Result<GenreDTO>> CreateGenreAsync(CreateGenreDTO createGenreDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteGenreAsync(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateGenreAsync(int ID, UpdateGenreDTO updateGenreDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
