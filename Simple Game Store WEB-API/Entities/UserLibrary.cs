e9c83d1683f11e3732e70bfe12e28344d4a7a43b
ArGul <argul.mail@gmail.com>
2026-04-17 12:32:20 +0300

    Created UserLibrary

---
+++ Simple Game Store WEB-API/Entities/UserLibrary.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_Game_Store_WEB_API.Entities
+{
+    public class UserLibrary // UserLibrary Entity Class - Represents A User's Game Library
+    {
+        public required int ID { get; set; }
+        public required User Owner { get; set; }
+        public required List<Game> Games { get; set; }
+    }
+}
