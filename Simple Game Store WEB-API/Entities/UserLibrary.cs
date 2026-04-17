9f096f6bb88af74f9a958a15cda68926a1a51f2a
ArGul <argul.mail@gmail.com>
2026-04-17 12:55:35 +0300

    Delete OwnerID from UserLibrary

--- Simple Game Store WEB-API/Entities/UserLibrary.cs
+++ Simple Game Store WEB-API/Entities/UserLibrary.cs
@@ -3,7 +3,6 @@
     public class UserLibrary // UserLibrary Entity Class - Represents A User's Game Library
     {
         public int ID { get; set; }
-        public required int OwnerID { get; set; }
         public List<Game> Games = new List<Game>();
     }
 }
