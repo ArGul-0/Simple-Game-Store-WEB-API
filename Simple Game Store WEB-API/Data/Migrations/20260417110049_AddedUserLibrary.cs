e0d236f48932052a92aab3ab143a7052fd8c5a68
ArGul <argul.mail@gmail.com>
2026-04-17 14:01:17 +0300

    dotnet ef migrations add AddedUserLibrary

---
+++ Simple Game Store WEB-API/Data/Migrations/20260417110049_AddedUserLibrary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Simple_Game_Store_WEB_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserLibrary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLibraries",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLibraries", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_UserLibraries_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGame",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LibraryID = table.Column<int>(type: "integer", nullable: false),
+                    UserLibraryID = table.Column<int>(type: "integer", nullable: false),
+                    GameID = table.Column<int>(type: "integer", nullable: false),
+                    PurchasedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGame", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_UserGame_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGame_UserLibraries_UserLibraryID",
+                        column: x => x.UserLibraryID,
+                        principalTable: "UserLibraries",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGame_GameID",
+                table: "UserGame",
+                column: "GameID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGame_UserLibraryID",
+                table: "UserGame",
+                column: "UserLibraryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLibraries_UserID",
+                table: "UserLibraries",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserGame");
+
+            migrationBuilder.DropTable(
+                name: "UserLibraries");
+        }
+    }
+}
