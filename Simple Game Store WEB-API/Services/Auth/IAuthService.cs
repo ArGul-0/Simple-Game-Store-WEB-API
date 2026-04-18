03b9ba094b974cca858614a7fc5721dfb318ec32
ArGul <argul.mail@gmail.com>
2026-04-18 16:28:34 +0300

    Added returns info in IAuthService

--- Simple Game Store WEB-API/Services/Auth/IAuthService.cs
+++ Simple Game Store WEB-API/Services/Auth/IAuthService.cs
@@ -8,10 +8,12 @@ namespace Simple_Game_Store_WEB_API.Services.Auth
         /// <summary>
         /// Login Is Used To Authenticate A User With The Provided Email And Password. It Validates The Credentials Against The User Store And Establishes An Authenticated Session If The Credentials Are Valid. If The Authentication Is Successful, It May Generate An Authentication Token Or Set A Cookie To Maintain The User's Authenticated State For Subsequent Requests.
         /// </summary>
+        /// <returns>A Result Object Containing A String Value, Which Could Be An Authentication Token Or A Success Message, Or An Error If The Authentication Fails.</returns>
         public Task<Result<string>> Login(string email, string password);
         /// <summary>
         /// Register Is Used To Create A New User Account With The Provided Username, Email, And Password. It Validates The Input Data, Ensures That The Username And Email Are Unique, And Stores The User's Credentials In The User Store. After Successful Registration, The User Can Log In Using The Registered Credentials To Access Protected Resources Or Perform Authenticated Actions Within The Application.
         /// </summary>
+        /// <returns>A Result Object Containing A String Value, Which Could Be A Success Message Or An Authentication Token If The Registration Is Successful, Or An Error If The Registration Fails Due To Validation Issues, Duplicate Username / Email, Or Other Errors.</returns>
         public Task<Result<string>> Register(string username, string email, string password);
     }
 }
