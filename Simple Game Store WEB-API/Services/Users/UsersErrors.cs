3f3a8164dbea7af592a7aaf12ecb79c3cd6a2978
ArGul <argul.mail@gmail.com>
2026-04-18 16:43:51 +0300

    Created UsersErrors and UserDontHavePermissionToPerformAction error.

---
+++ Simple Game Store WEB-API/Services/Users/UsersErrors.cs
@@ -0,0 +1,9 @@
+﻿using Simple_Game_Store_WEB_API.Common.Results;
+
+namespace Simple_Game_Store_WEB_API.Services.Users
+{
+    public static class UsersErrors
+    {
+        public static Error UserDontHavePermissionToPerformAction = new Error("UserDontHavePermissionToPerformAction", "You don't have permission to perform this action.");
+    }
+}
