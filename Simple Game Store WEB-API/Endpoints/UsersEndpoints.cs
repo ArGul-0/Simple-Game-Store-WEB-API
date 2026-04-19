5653b37d1aa75e1f4fcd1d8e6f060f4bc7605a34
ArGul <argul.mail@gmail.com>
2026-04-19 16:49:17 +0300

    Added .WithName in UsersEndpoints.

--- Simple Game Store WEB-API/Endpoints/UsersEndpoints.cs
+++ Simple Game Store WEB-API/Endpoints/UsersEndpoints.cs
@@ -11,6 +11,11 @@ namespace Simple_Game_Store_WEB_API.Endpoints
 {
     public static class UsersEndpoints // Static Class For User Endpoints
     {
+        const string GetMyIDEndpointName = "GetMyID"; // Constant For The Get My ID Endpoint Name
+        const string GetGamesFromLibraryEndpointName = "GetGamesFromLibrary"; // Constant For The Get Games From Library Endpoint Name
+        const string AddGameToUserLibraryEndpointName = "AddGameToUserLibrary"; // Constant For The Add Game To User Library Endpoint Name
+        const string RemoveGameFromUserLibraryEndpointName = "RemoveGameFromUserLibrary"; // Constant For The Remove Game From User Library Endpoint Name
+
         public static RouteGroupBuilder MapUsersEndpoints(this WebApplication app)
         {
             var usersGroup = app.MapGroup("/Users/"); // Create A Group For /Users Endpoints
@@ -21,7 +26,7 @@ namespace Simple_Game_Store_WEB_API.Endpoints
             usersGroup.MapGet("/GetMyID", async (HttpContext httpContext) =>
             {
                 return Results.Ok(int.Parse(httpContext.User.FindFirst("userID")?.Value ?? "0"));
-            }).RequireAuthorization();
+            }).WithName(GetMyIDEndpointName).RequireAuthorization();
 
             // Get All User's Games From Library
             usersGroup.MapGet("/{userID}/GetGamesFromLibrary", async (int userID, GameStoreContext dbContext, IUsersMapper usersMapper, HttpContext httpContext) =>
@@ -43,7 +48,7 @@ namespace Simple_Game_Store_WEB_API.Endpoints
                 List<UserGameDTO> games = userLibrary.Games.Select(ug => usersMapper.ToDTO(ug)).ToList();
 
                 return Results.Ok(games);
-            }).RequireAuthorization();
+            }).WithName(GetGamesFromLibraryEndpointName).RequireAuthorization();
 
             // Add A Game To The Current User's Library
             usersGroup.MapPost("/{userID}/AddGameToUserLibrary", async (int userID, int gameID, IUserService userService, HttpContext httpContext) =>
@@ -67,7 +72,7 @@ namespace Simple_Game_Store_WEB_API.Endpoints
 
                 return Results.Ok();
                     
-            }).RequireAuthorization();
+            }).WithName(AddGameToUserLibraryEndpointName).RequireAuthorization();
 
             // Remove A Game From The Current User's Library
             usersGroup.MapDelete("/{userID}/RemoveGameFromLibrary", async (int userID, int gameID, IUserService userService, HttpContext httpContext) =>
@@ -89,7 +94,7 @@ namespace Simple_Game_Store_WEB_API.Endpoints
             }
 
             return Results.Ok();
-            }).RequireAuthorization();
+            }).WithName(RemoveGameFromUserLibraryEndpointName).RequireAuthorization();
 
             return usersGroup; // Return The Group For Further Configuration If Needed
         }
